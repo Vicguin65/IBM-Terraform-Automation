ee78a0708b5d3dee788c877d7a00e3061a6cbbab
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-06-14 15:43:33 -0400

    minor naming conventions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,13 @@
 provider "aws"{
-    //externalized
     access_key = ""
     secret_key = ""
     region = "us-west-1"
 }
-resource "aws_instance" "hello"{
+resource "aws_instance" "hello_terraform_instance"{
     ami = "ami-0f66240e199159416"
     instance_type = "t2.micro"
     tags = {
-        Name = "hello_terraform"
+        Name = "hello_world_terraform"
     }
 }
-
-//masking all the keys
-//input 
-//output
 
