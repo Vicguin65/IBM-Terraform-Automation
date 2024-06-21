79b2d284c6c013d644950a3e6a75015a8be3102b
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-06-21 15:23:41 -0400

    Added starting infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,61 @@
 provider "aws"{
     access_key = ""
     secret_key = ""
-    region = "us-west-1"
-}
-resource "aws_instance" "hello_terraform_instance"{
-    ami = "ami-0f66240e199159416"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "hello_world_terraform"
-    }
+    region = "us-west-2"
 }
 
+resource "aws_instance" "instance_one"{
+    ami = "ami-0cf2b4e024cdb6960"
+    instance_type = "t2.micro"
+    
+    network_interface {
+      network_interface_id = aws_network_interface.first_interface.id
+      device_index         = 0
+  }
+}
+
+resource "aws_instance" "instance_two"{
+    ami = "ami-0cf2b4e024cdb6960"
+    instance_type = "t2.micro"
+    
+    network_interface {
+      network_interface_id = aws_network_interface.second_interface.id
+      device_index         = 0
+  }
+}
+
+
+resource "aws_vpc" "vpc_main" {
+
+  cidr_block = "10.0.0.0/16"
+  tags={
+    Name="terraform created VPC"
+  }
+}
+
+resource "aws_subnet" "subnet_one" {
+  vpc_id     = aws_vpc.vpc_main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+
+}
+
+resource "aws_subnet" "subnet_two" {
+  vpc_id     = aws_vpc.vpc_main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_subnet" "subnet_three" {
+  vpc_id     = aws_vpc.vpc_main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-west-2c"
+}
+
+resource "aws_network_interface" "first_interface" {
+  subnet_id   = aws_subnet.subnet_one.id
+}
+
+resource "aws_network_interface" "second_interface" {
+  subnet_id   = aws_subnet.subnet_two.id
+}
