82a12a1f074cab13af7caebf5b2ae7d56cb09b1d
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-06-27 23:20:01 -0400

    fixed route table problems

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,12 @@ provider "aws"{
     secret_key = ""
     region = "us-west-2"
 }
-
-resource "aws_instance" "instance_one"{
-    ami = "ami-0cf2b4e024cdb6960"
+resource "aws_instance" "hello_terraform_instance"{
+    ami = "ami-0f66240e199159416"
     instance_type = "t2.micro"
     user_data = "${file("user-data-apache.sh")}"
+
+    depends_on = [aws_internet_gateway.internet_gateway]
 
     network_interface {
       network_interface_id = aws_network_interface.first_interface.id
@@ -37,29 +38,19 @@ resource "aws_internet_gateway" "internet_gateway" {
   vpc_id = aws_vpc.vpc_main.id
 }
 
-resource "aws_route_table" "route_table" {
-  vpc_id = aws_vpc.vpc_main.id
+resource "aws_default_route_table" "our_default_route_table" {
+  default_route_table_id = aws_vpc.vpc_main.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
 }
 
-resource "aws_eip" "one" {
-  domain                    = "vpc"
-  network_interface         = aws_network_interface.first_interface.id
-}
-
-resource "aws_nat_gateway" "example" {
-  subnet_id     = aws_subnet.subnet_one.id
-  allocation_id = aws_eip.one.id
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.internet_gateway]
-}
-
-resource "aws_route" "r1" {
-  route_table_id            = aws_route_table.route_table.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.example.id
-}
+# resource "aws_route" "r1" {
+#   route_table_id            = aws_route_table.route_table.id
+#   destination_cidr_block    = "0.0.0.0/0"
+# }
 
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
@@ -67,16 +58,11 @@ resource "aws_security_group" "allow_http" {
   vpc_id      = aws_vpc.vpc_main.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_http.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-
 resource "aws_subnet" "subnet_one" {
   vpc_id     = aws_vpc.vpc_main.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-west-2a"  
+  map_public_ip_on_launch = true
 
 }
 
