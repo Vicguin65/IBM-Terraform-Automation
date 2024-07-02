6a0e1d21b1a2762eb0834bb015aebdbf67f7aad4
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-07-02 00:39:45 -0400

    Update user-data-apache.sh

--- terraform/user-data-apache.sh
+++ terraform/user-data-apache.sh
@@ -7,3 +7,4 @@ sudo git checkout crime-data-analysis
 sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo cp -a ./liedetector/. /var/www/html/
+sudo echo "<h1> Accessed from $(hostname -f)</h1>" >> /var/www/html/index.html
