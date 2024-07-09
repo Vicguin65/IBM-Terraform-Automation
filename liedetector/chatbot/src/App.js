5062bd6ea7b44be117d89dd148a2376a79d1f608
hazelyu13 <hazbae13@gmail.com>
2024-07-09 15:18:10 -0400

    chatbot

---
+++ liedetector/chatbot/src/App.js
@@ -0,0 +1,17 @@
+// src/App.js
+import React from 'react';
+import Chatbot from './Chatbot';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Chatbot</h1>
+        <Chatbot />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
