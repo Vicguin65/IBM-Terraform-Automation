e4874609af84d12fd795c95447deaa4e2525b9b9
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-07-12 15:23:44 -0400

    Complete Refactor into Modules

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,24 @@
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR Blocks for subnets"
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR Blocks for subnets"
+  type = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
