e0c9da1513b8503f1bc17431fc874d4df1ddad50
Tyler Du <35591950+Vicguin65@users.noreply.github.com>
2024-07-19 15:11:05 -0400

    Created s3-bucket to save state
    I created an S3 bucket and DynamoDB table on AWS. This allowed me to create an S3 backend for the project.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,24 @@
-# TODO
-# ADD SECRETS INSTEAD OF HARDCODE
-# This task is related to just setting up variables in general
+# tfstate files are stored on S3 buckets
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.58.0"
+    }
+  }
+
+  # backend specifies where the state files are stored
+  backend "s3" {
+    bucket = "rcos-terraform"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    
+    # this table specifies where a lock is created to prevent a race condition
+    dynamodb_table = "terraform_tf_lockid"
+  }
+}
+
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
