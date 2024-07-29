804a5a943dcc173b12b23d4b1c964d90e7d7ab9b
ArnavM34 <116925124+ArnavM34@users.noreply.github.com>
2024-07-29 12:07:28 -0400

    Added links to resources
    Links work for all resources

--- lie_detect/src/Resources.js
+++ lie_detect/src/Resources.js
@@ -1,6 +1,14 @@
 // Resources.js
 import React from 'react';
 import './styles.css'; // Make sure to include your CSS file
+import {Link} from 'react-router-dom';
+
+const links = [
+  { url: 'https://nap.nationalacademies.org/read/10420/chapter/7'},
+  { url: 'https://www.mdpi.com/2078-2489/14/10/564'},
+  { url: 'https://link.springer.com/article/10.1007/s11042-023-16769-w'},
+  { url: 'https://pubmed.ncbi.nlm.nih.gov/31282623/'},
+];
 
 const Resources = () => {
   return (
@@ -13,22 +21,22 @@ const Resources = () => {
           “The Polygraph and Lie Detection" at Nap.Edu.” 5 Evidence
            from Polygraph Research: Quantitative Assessment | The Polygraph 
            and Lie Detection | The National Academies Press, National Academic Press,
-          2003, nap.nationalacademies.org/read/10420/chapter/7. Accessed 27 June 2024.
+          2003, <a target="_blank" href={links[0]['url']}>{links[0]['url']}</a>. Accessed 27 June 2024.
           </p>
           <p className="resources">
           Rad, Dana, et al. “Neural Network Applications in Polygraph Scoring-a Scoping Review.”
           MDPI, Multidisciplinary Digital Publishing Institute, 13 Oct. 2023, 
-          www.mdpi.com/2078-2489/14/10/564. Accessed 27 June 2024.
+          <a target="_blank" href={links[1]['url']}>{links[1]['url']}</a>. Accessed 27 June 2024.
           </p>
           <p className="resources">
           Talaat, Fatma M. “Explainable Enhanced Recurrent Neural Network for Lie Detection Using 
           Voice Stress Analysis - Multimedia Tools and Applications.” SpringerLink, Springer US, 
-          20 Sept. 2023, link.springer.com/article/10.1007/s11042-023-16769-w. Accessed 27 June 2024.
+          20 Sept. 2023, <a target="_blank" href={links[2]['url']}>{links[2]['url']}</a>. Accessed 27 June 2024.
           </p>
           <p className="resources">
           Sun, Z Y, et al. “Polygraph Accuracy of Control Question Test in Criminal Cases.” 
           Polygraph Accuracy of Control Question Test in Criminal Cases, U.S. National Library of 
-          Medicine, 25 June 2019, pubmed.ncbi.nlm.nih.gov/31282623/. Accessed 27 June 2024.
+          Medicine, 25 June 2019, <a target="_blank" href={links[3]['url']}>{links[3]['url']}</a>. Accessed 27 June 2024.
           </p>
         </div>
       </section>
