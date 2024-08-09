7f06d13e92ed8b875e13dbd7ed7e8b22c8402fcc
ArnavM34 <116925124+ArnavM34@users.noreply.github.com>
2024-08-09 15:12:16 -0400

    hazel fizx
    change team names

--- lie_detect/src/MeetTheTeam.js
+++ lie_detect/src/MeetTheTeam.js
@@ -4,25 +4,25 @@ import "./MeetTheTeam.css"; // Import CSS for this component
 const teamMembers = [
   {
     name: "Hazel Yu",
-    title: "Project Manager & Frontend Engineer",
+    title: "Project Manager & UX Designer",
     photo: require("./assets/IMG_2183.jpeg"),
     bio: "Hi I’m Hazel, I am a Computer Science Major at RPI with a minor in Economics and Psychology. This is my first semester with the Rensselaer Center for Open Source. During my free time I like to shop, read books, watch netflix, and go on TikTok. I love thrifting anywhere I am so if you have any recommendations please let me know!"
   },
   {
     name: "Tyler Du",
-    title: "DevOps Engineer",
+    title: "DevOps Engineer & Network Engineer",
     photo: require("./assets/IMG_8934.jpeg"),
     bio: "Hi, this is Tyler! I am an undergraduate studying Computer Science graduating in Spring 2025. I am from the Biggest Little City in the World, Reno, Nevada[https://www.reno.gov/]. This is my third semester with RCOS! I spend lots of my free time playing a trading card game called Cardfight Vanguard[https://en.cf-vanguard.com/]."
   },
   {
     name: "Ritika Brahmadesam",
-    title: "DevOps Engineer",
+    title: "DevOps Engineer & Network Engineer",
     photo: require("./assets/IMG_7842_copy_2.jpg"),
     bio: "Hi I’m Ritika, I am a Mathematics and Computer Science Major at RPI with a minor in Economics. This is my third semester with the Rensselaer Center for Open Source. During my free time I like to swim, play my violin, and read. I am currently reading The Way of Kings by Brandon Sanderson. I really enjoy getting a sweet treat, specifically anything with peanut butter or chocolate, or even better both!"
   },
   {
     name: "Arnav Mukherjee",
-    title: "Data Scientist",
+    title: "Data Engineer",
     photo: require("./assets/IMG_3173.jpeg"),
     bio: "Hi, this is Arnav, I am an undergraduate Games Simulation Arts and Sciences and Computer Science major. I am from Pleasanton, CA and this is my first semester with RCOS. I enjoy running, hiking, traveling, making art, and playing video games. My favorite video game is Mario Kart Wii (I grew up with it). "
   }
